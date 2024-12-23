7daab8635f0e63b78ab36da8e7b3de3ca3c1f010
Armstain <tahsin10@outlook.com>
2024-12-24 00:01:15 +0600

    added api for search and autocomplete

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,9 +13,42 @@ const apiClient = axios.create({
         'SECRET-CODE': SECRET_CODE
     }
 });
+// search flight params
+export type SearchFlightParams = {
+    journey_type: 'OneWay' | 'RoundTrip' | 'MultiCity';
+    segment: {
+        departure_airport_type: 'CITY' | 'AIRPORT';
+        departure_airport: string;
+        arrival_airport_type: 'CITY' | 'AIRPORT';
+        arrival_airport: string;
+        departure_date: string;
+    }[];
+    travelers_adult: number;
+    travelers_child: number;
+    travelers_child_age: number;
+    travelers_infants: number;
+    travelers_infants_age: string[];
+    preferred_carrier: (string | null)[];
+    non_stop_flight: 'any' | 'non-stop';
+    baggage_option: 'any' | 'only-baggage';
+    booking_class: 'Economy' | 'Premium-Economy' | 'Business' | 'First-Class';
+    supplier_uid: string;
+    partner_id: string;
+    language: string;
+}
 
-// fetch airport Data for autocomplete
-export const fetchAirportData = async (): Promise<any> => {
+// search flight
+export const searchFlight = async (params: SearchFlightParams): Promise<any> => {
+    try {
+        const response = await apiClient.post("/tools/flight-search", params);
+        return response.data;
+    } catch (error) {
+        console.error("Error searching flights:", error);
+        throw error;
+    }
+}
+// get auto suggestion for airport
+export const getAutoSuggestionForAirport = async (): Promise<any> => {
     try {
         const response = await apiClient.get("/tools/airport-autosuggetion-data");
         return response.data;
@@ -23,4 +56,4 @@ export const fetchAirportData = async (): Promise<any> => {
         console.error("Error fetching airport data:", error);
         throw error;
     }
-};
+}
